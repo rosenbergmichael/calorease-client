cdfd574f13946927294cd0517fe6a3c108256bd9
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-11-06 13:43:24 -0500

    set up FoodsContainer.js, imported in Tracker.js component

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import {Redirect} from 'react-router-dom'
 
+import FoodsContainer from '../containers/FoodsContainer'
 
 const Tracker = (props) => {
 
   let tracker = props.trackers[props.match.params.id - 1]
 
   return (
-    <h2>
-      {/* {tracker ? null : <Redirect to='/trackers'/> } */}
-      <strong>Day: </strong>{tracker ? tracker.day : null} | <strong>Date: </strong>{tracker ? tracker.date : null} | <strong>Total Calories: </strong>{tracker ? tracker.totalcal : null}
-      {/* <strong>Day: </strong>{props.tracker.day} | <strong>Date: </strong> {props.tracker.date} | <strong>Total Calories: </strong>{props.tracker.totalcal} */}
-    </h2>
+    <div>
+      <h2>
+        {/* {tracker ? null : <Redirect to='/trackers'/> } */}
+        <strong>Day: </strong>{tracker ? tracker.day : null} | <strong>Date: </strong>{tracker ? tracker.date : null} | <strong>Total Calories: </strong>{tracker ? tracker.totalcal : null}
+        {/* <strong>Day: </strong>{props.tracker.day} | <strong>Date: </strong> {props.tracker.date} | <strong>Total Calories: </strong>{props.tracker.totalcal} */}
+      </h2>
+      <FoodsContainer/>
+    </div>
+    
   )
    
 }
---
+++ src/containers/FoodsContainer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+class FoodsContainer extends React.Component {
+
+  render() {
+    return (
+      <div>
+        FoodsContainer
+      </div>
+    )
+
+  }
+
+
+}
+
+
+export default FoodsContainer
