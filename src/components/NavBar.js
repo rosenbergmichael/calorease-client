d692df5b1fc753e7ca0f37758d53b0b89f499a89
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-09 17:10:38 -0500

    add and edit styling of NavBar in style.css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,14 +7,16 @@ const NavBar = (props) => {
   return (
     <div className="NavBar">
       <header> 
+        <div className="overlay">
         <ul>
-          <img src="https://betsysbest.com/wp-content/uploads/2019/09/food-cost-calculation-for-a-restaurant-1080x675.jpg"></img>
+          {/* <img src="https://betsysbest.com/wp-content/uploads/2019/09/food-cost-calculation-for-a-restaurant-1080x675.jpg"></img> */}
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/trackers'>View All</Link></li>
           <li><Link to='/trackers/new'>Create</Link></li>
           <li><Link to='/mostrecent'>Most Recent Entry</Link></li>
           
         </ul>
+        </div>
       </header>
     </div>
   )
